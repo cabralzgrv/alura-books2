f3a865899c98be81f77f40976c2eadbdf9330e6e
Lucas Cabral <cabralz18@outlook.com>
2023-09-06 01:15:36 -0300

    Links commit

--- src/componentes/Header/index.jsx
+++ src/componentes/Header/index.jsx
@@ -2,6 +2,7 @@ import Logo from '../../componentes/Logo';
 import IconesHeader from '../../componentes/IconesHeader';
 import OpcoesHeader from '../../componentes/OpcoesHeader';
 import styled from 'styled-components'
+import { Link } from 'react-router-dom';
 
 const HeaderContainer = styled.header`
     background-color: #FFFFFF;
@@ -13,7 +14,9 @@ const HeaderContainer = styled.header`
 function Header () {
     return (
         <HeaderContainer>
-            <Logo/>
+            <Link to="/">
+                <Logo/>
+            </Link>
             <OpcoesHeader/>
             <IconesHeader/>
         </HeaderContainer>
--- src/rotas/Favoritos.jsx
+++ src/rotas/Favoritos.jsx
@@ -1,6 +1,5 @@
-import Header from './componentes/Header';
-import SearchBar from './componentes/Search';
-import UltimosLancamentos from './componentes/UltimosLancamentos';
+import SearchBar from '../componentes/Search';
+import UltimosLancamentos from '../componentes/UltimosLancamentos';
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
@@ -9,14 +8,13 @@ const AppContainer = styled.div`
   background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
 `
 
-function App() {
+function Favoritos() {
   return (
     <AppContainer>
-      <Header/>
       <SearchBar/>
       <UltimosLancamentos/>
     </AppContainer>
   );
 }
 
-export default App;
+export default Favoritos;
